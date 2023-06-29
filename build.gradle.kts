1c5f574b845f98ce63f4415c15a02e7f9ffe51e2
JanMaki <mutsukimaki@gmail.com>
2023-06-29 17:43:25 +0900

    更新 もろもろ更新
    paperのバージョンを更新
    maven publishを削除

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,18 +8,18 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://libraries.minecraft.net")
 }
 
 dependencies {
     compileOnly("com.mojang:brigadier:1.0.18")
-    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT")
 }
 
 tasks {
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 
     create<Copy>("buildPlugin") {
@@ -39,20 +39,5 @@ tasks {
     artifacts {
         archives(sourcesJar)
         archives(javadocJar)
-    }
-}
-
-mavenPublish {
-    sonatypeHost = com.vanniktech.maven.publish.SonatypeHost.S01
-}
-publishing {
-    repositories {
-        maven {
-            url = if(version.toString().endsWith("SNAPSHOT")) {
-                uri("$buildDir/repos/snapshots")
-            } else {
-                uri("$buildDir/repos/releases")
-            }
-        }
     }
 }
